9af9f4b834c98c68e0cb48e260b8b60a8b424c6b
SAlsalamah <Salsalamah@mail.stmarytx.edu>
2022-10-27 19:47:46 -0500

    The Second version. Good Job Saleh!

--- app/src/main/java/com/example/foodbank/MainActivity.kt
+++ app/src/main/java/com/example/foodbank/MainActivity.kt
@@ -14,17 +14,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // drawer layout instance to toggle the menu icon to open
-        // drawer and back button to close drawer
+
         drawerLayout = findViewById(R.id.my_drawer_layout)
         actionBarDrawerToggle = ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close)
 
-        // pass the Open and Close toggle for the drawer layout listener
-        // to toggle the button
+
         drawerLayout.addDrawerListener(actionBarDrawerToggle)
         actionBarDrawerToggle.syncState()
 
-        // to make the Navigation drawer icon always appear on the action bar
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
